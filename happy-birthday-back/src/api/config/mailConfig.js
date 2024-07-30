1981448d4bcddeea212f02db7fccdd8ba7c6e747
Tardisdoc12 <jean.anquetil@livecampus.tech>
2024-07-30 12:49:33 +0200

    suppression des mot de passe en clair

--- happy-birthday-back/src/api/config/mailConfig.js
+++ happy-birthday-back/src/api/config/mailConfig.js
@@ -5,8 +5,8 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   service: 'Gmail',
   auth: {
-    user: 'jean.anquetil12@gmail.com',
-    pass: 'Mds1f2mp!je99',
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
   },
 });
 
